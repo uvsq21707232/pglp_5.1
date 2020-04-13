67085bd4ae872c1165bb5dea90f25e904fe105e9
abdelhafid belhabib <abdelhafid.belhabib@ens.uvsq.fr>
2020-04-13 23:28:27 +0200

    implementation de la fabrique pour GrouprPersonnel

--- pglp_5_1/src/main/java/Dao/GroupePersonnelDAO.java
+++ pglp_5_1/src/main/java/Dao/GroupePersonnelDAO.java
@@ -1,40 +1,53 @@
 package Dao;
 
 import java.util.ArrayList;
-
 import Abdelhafid.pglp_5_1.Personnel.GroupePersonnel;
+
 
 public class GroupePersonnelDAO implements Dao<Abdelhafid.pglp_5_1.Personnel.GroupePersonnel> {
 
 	
+	private ArrayList<GroupePersonnel> liste_personnels;
+	public GroupePersonnelDAO() {
+		
+		this.liste_personnels= new ArrayList<GroupePersonnel>();
+	}
+	
 	@Override
 	public void ajouter(GroupePersonnel object) {
-		// TODO Auto-generated method stub
+	liste_personnels.add(object);
 		
 	}
 
 	@Override
 	public GroupePersonnel find(int id) {
-		// TODO Auto-generated method stub
+		for ( GroupePersonnel gp : liste_personnels) {
+            if (gp.getId_composite() == id) {
+                return gp;
+            }
+        }
+		
 		return null;
 	}
 
 	@Override
 	public ArrayList<GroupePersonnel> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+	
+		
+		return (ArrayList<GroupePersonnel>) liste_personnels.clone();
 	}
 
 	@Override
 	public GroupePersonnel update(GroupePersonnel obj) {
-		// TODO Auto-generated method stub
+		
+		
 		return null;
 	}
 
 	@Override
 	public void delete(GroupePersonnel obj) {
-		// TODO Auto-generated method stub
-
+		
+		liste_personnels.remove(obj);
 	}
 
 	
