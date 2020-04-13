2f09df274746d4d026d34b8ef908816aa4b93927
abdelhafid belhabib <abdelhafid.belhabib@ens.uvsq.fr>
2020-04-13 19:41:30 +0200

    ajout de la fabrique de Affichage par groupe

---
+++ pglp_5_1/src/main/java/Dao/AffichageParGroupeDAO.java
@@ -0,0 +1,39 @@
+package Dao;
+
+import java.util.ArrayList;
+
+import Abdelhafid.pglp_5_1.Personnel.AffichageParGroupe;
+
+public class AffichageParGroupeDAO implements Dao<Abdelhafid.pglp_5_1.Personnel.AffichageParGroupe> {
+
+	@Override
+	public void ajouter(AffichageParGroupe object) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public AffichageParGroupe find(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<AffichageParGroupe> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AffichageParGroupe update(AffichageParGroupe obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(AffichageParGroupe obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
