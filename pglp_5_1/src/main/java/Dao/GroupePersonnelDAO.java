ff85b817ec8f2578e50db4dbf23d7142df3cb636
abdelhafid belhabib <abdelhafid.belhabib@ens.uvsq.fr>
2020-04-13 19:40:40 +0200

    ajout de la fabrique de GroupePersonnel

---
+++ pglp_5_1/src/main/java/Dao/GroupePersonnelDAO.java
@@ -0,0 +1,42 @@
+package Dao;
+
+import java.util.ArrayList;
+
+import Abdelhafid.pglp_5_1.Personnel.GroupePersonnel;
+
+public class GroupePersonnelDAO implements Dao<Abdelhafid.pglp_5_1.Personnel.GroupePersonnel> {
+
+	
+	@Override
+	public void ajouter(GroupePersonnel object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public GroupePersonnel find(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<GroupePersonnel> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public GroupePersonnel update(GroupePersonnel obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(GroupePersonnel obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+
+}
